83ac9dc1645932a6f4fbb1221315479c983c65b9
James Wu <ywu@lcs.on.ca>
2019-11-21 20:24:16 -0500

    Initial Commit

---
+++ FederalTaxCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FederalTaxCalculator
+//
+//  Created by James Wu on 2019-11-21.
+//  Copyright © 2019 James Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
